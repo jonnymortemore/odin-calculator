c41ecf3f6f6c9cce508c8ec2c3bd55d1d08ec902
Jonny Mortemore <jonathanmortemore@gmail.com>
2025-03-04 17:27:30 +1100

    Add event listener for decimal button
    add event listener to decimal button to add "." to calc object and
    display only when object values are appropriate

--- code.js
+++ code.js
@@ -23,8 +23,8 @@ function divide(a, b) {
 
 function operator(a, b, operator) {
     let result = 0
-    a = parseInt(a)
-    b = parseInt(b)
+    a = parseFloat(a)
+    b = parseFloat(b)
     switch(operator) {
         case "+":
             result = add(a, b);
@@ -95,7 +95,7 @@ document.querySelector('.equals').addEventListener('click', () => {
         sum = operator(calculatorValues.number1, calculatorValues.number2, operand=calculatorValues.operand);
         displayValue(sum, reset=true);
         calculatorValues.number1 = sum;
-        calculatorValues.number2 = 0
+        calculatorValues.number2 = ""
         calculatorValues.operand = ""
     }
 })
@@ -103,7 +103,20 @@ document.querySelector('.equals').addEventListener('click', () => {
 //clear button
 document.querySelector('.clear').addEventListener('click', () => {
     displayValue(0, reset=true);
-    calculatorValues.number1 = 0;
-    calculatorValues.number2 = 0
+    calculatorValues.number1 = "";
+    calculatorValues.number2 = ""
     calculatorValues.operand = ""
+})
+
+//decimal button
+
+document.querySelector('.decimal').addEventListener('click', () => {
+    if (calculatorValues.number1 != 0 && calculatorValues.operand == "") {
+        displayValue(".", reset=false);
+        calculatorValues.number1 += "."
+    } else if (calculatorValues.number2 != 0 && calculatorValues.operand != "") {
+        displayValue(".", reset=false);
+        calculatorValues.number2 += "."
+    }
+    
 })
